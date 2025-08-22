14fc7564b19802ba2bc8f718bb9a48baa6cc0d4e
Uttkarsh kesharwani <145669545+UttkarshKesharwani@users.noreply.github.com>
2025-08-22 17:35:46 +0530

    Time: 0 ms (100%), Space: 10.2 MB (73.57%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,23 +2,40 @@ class Solution {
 public:
 
     // [2,1,1,9];
-    
+
     vector<int> dp;
 
-    int f(vector<int>& nums,int i){
+    int ftd(vector<int>& nums,int i){
 
         if(i==nums.size()-1) return nums[i];
         if(i==nums.size()-2) return max(nums[i],nums[i+1]);
 
         if(dp[i]!=-1) return dp[i];
 
-        return dp[i] = max( nums[i]+f(nums,i+2) , f(nums,i+1) );
+        return dp[i] = max( nums[i]+ftd(nums,i+2) , 0+ ftd(nums,i+1) );
+    }
+
+    // bottom up
+    int fbu(vector<int>& nums){
+
+        int n=nums.size();
+        if(n==1) return nums[0];
+
+        dp[n-1]=nums[n-1];
+        dp[n-2] = max(nums[n-2],nums[n-1]);
+
+        for(int i=n-3;i>=0;i--){
+            dp[i] = max( nums[i]+dp[i+2] , 0+dp[i+1]);
+        }
+
+        return dp[0];
     }
 
     int rob(vector<int>& nums) {
         dp.clear();
         dp.resize(105,-1);
-        int idx = 0; 
-        return f(nums,idx);
+        // int idx = 0; 
+        // return ftd(nums,idx);
+        return fbu(nums);
     }
 };
