4877de5d66d638df96da2abedad799de8cfebff0
Uttkarsh kesharwani <145669545+UttkarshKesharwani@users.noreply.github.com>
2025-09-26 02:03:12 +0530

    Time: 0 ms (100%), Space: 11.3 MB (48.27%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -1,18 +1,39 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int sum=0;
-    void reverseInorder(TreeNode* root){
-        // right root left
-        if(root == NULL ) return ;
-        reverseInorder(root->right);
-        root->val = root->val + sum;
-        sum=root->val;
-        reverseInorder(root->left);
-
+    int i=0;
+    void inOrder(TreeNode* root , vector<int>& ans){
+        if(root == NULL) return;
+        inOrder(root->left,ans);
+        ans.push_back(root->val);
+        inOrder(root->right,ans);
     }
+
+    void changeValueOfBST(TreeNode* root,vector<int>& ans){
+        if(root == NULL) return;
+        changeValueOfBST(root->left,ans);
+        root->val = ans[i++];
+        changeValueOfBST(root->right,ans);
+    }
+
     TreeNode* bstToGst(TreeNode* root) {
-        reverseInorder(root);
+        vector<int> ans;
+        inOrder(root,ans);
+
+        for(int j=ans.size()-2;j>=0;j--) ans[j] = ans[j]+ans[j+1];
+
+        changeValueOfBST(root,ans);
+
         return root;
     }
 };
