4e8445d004cc46af8c65233363ca161a1ccfa0a3
Uttkarsh kesharwani <145669545+UttkarshKesharwani@users.noreply.github.com>
2025-11-10 19:29:34 +0530

    Time: 27 ms (7.03%), Space: 22.9 MB (99.97%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        
-        vector<vector<int>> ans;
-        int n = intervals.size();
 
-        sort(intervals.begin(), intervals.end()); // sort by start time
 
-        int i = 0;
-        while (i < n) {
-            int start = intervals[i][0];
-            int end = intervals[i][1];
+        /*
+            Intution:- Just checking whether the next starting time is less than the current end time then overlapping exits , then merge the array 
+        */
+        vector<vector<int>> merged;
 
-            int j = i + 1;
-            // keep merging while next interval overlaps
-            while (j < n && end >= intervals[j][0]) {
-                end = max(end, intervals[j][1]);
-                j++;
+
+        sort(intervals.begin(),intervals.end());
+
+        merged.push_back(intervals[0]);
+
+        for(const auto& val:intervals){
+  
+            // there is overlap , then merge it
+            if( merged.back()[1] >= val[0] ){
+                merged.back()[1] = max(merged.back()[1],val[1]);  // ya jo pichle submission me max() wala likha hai vahi sahi (val[i] galat hai kuch testcase k liye)
+            }else{ // there is no overlap
+                merged.push_back(val);
             }
-
-            ans.push_back({start, end});
-            i = j; // move to the next unmerged interval
         }
 
-        return ans;
+        return merged;
     }
 };
