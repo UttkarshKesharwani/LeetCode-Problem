e60411579e5cae3af3334b5cba0f6c6fe69fd949
Uttkarsh kesharwani <145669545+UttkarshKesharwani@users.noreply.github.com>
2025-12-03 20:20:22 +0530

    Time: 17 ms (18.06%), Space: 46.3 MB (8.01%) - LeetHub

--- 3446-sort-matrix-by-diagonals/3446-sort-matrix-by-diagonals.cpp
+++ 3446-sort-matrix-by-diagonals/3446-sort-matrix-by-diagonals.cpp
@@ -2,35 +2,34 @@ class Solution {
 public:
     vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
 
+        // do also check the next submission
+
         int rows = grid.size();
         int cols = grid[0].size();
         vector<vector<int>> ans(rows,vector<int>(cols,0));
 
-        // mp[key] = { vector_of_values, index_pointer }
-        unordered_map< int, pair<vector<int>,int> > mp;
+        unordered_map<int,vector<int>> mp;
 
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
-                int key = i-j;
-                mp[key].first.push_back(grid[i][j]);
-                mp[key].second = 0;
+                mp[i-j].push_back(grid[i][j]);
             }
         }
 
+        // we do reverse enginnering here
+        // we need the front element and then we need to pop the front element which take linear time 
         for(auto &ele:mp){
             if(ele.first>=0){
-                sort(ele.second.first.rbegin(),ele.second.first.rend());
+                sort(ele.second.begin(),ele.second.end());
             }else{
-                sort(ele.second.first.begin(),ele.second.first.end());
+                sort(ele.second.rbegin(),ele.second.rend());
             }
         }
 
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
-                int key = i-j;
-                int idx = mp[key].second;
-                ans[i][j] = mp[key].first[idx];
-                mp[key].second++;
+                ans[i][j] = mp[i-j].back();
+                mp[i-j].pop_back();
             }
         }
 
