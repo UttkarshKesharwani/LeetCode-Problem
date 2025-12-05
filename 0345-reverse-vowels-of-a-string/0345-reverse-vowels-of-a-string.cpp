b88900d88fe6ec379064da6cdefd48f72b289e6a
Uttkarsh kesharwani <145669545+UttkarshKesharwani@users.noreply.github.com>
2025-12-05 21:09:27 +0530

    Time: 2 ms (46.45%), Space: 11.2 MB (9.16%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,8 +1,6 @@
 class Solution {
-public:
-
-    unordered_set<int> visited; // help to store the idx of the vowel present at the end 
-      
+public:      
+    
     bool checkIfVowel(char ch){
         if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
             return true;
@@ -10,29 +8,26 @@ public:
         return false;
     }
 
-    int findVowelFromLast(string s,int n){
-        for(int i=n-1;i>=0;i--){
-            if(not visited.count(i) && checkIfVowel(s[i])){
-                visited.insert(i);
-                return i;
-            }
-        }
-        return -1;
-    }
-
     string reverseVowels(string s) {
 
         int n=s.size();
+        deque<int> dq; // deque is used because we need to process the element from starting as well as from the end simultaneously
 
         for(int i=0;i<n;i++){
             if(checkIfVowel(s[i])){
-                int idx = findVowelFromLast(s,n);
-                if(idx == -1) return s;
-                if(idx == i) continue; 
-                if(i > idx) break;
-                swap(s[i],s[idx]);
+                dq.push_back(i);
             }
         }
+
+        int size = dq.size();
+           while(dq.size() > 1){
+            int idx1 = dq.front();
+            int idx2 = dq.back();
+            dq.pop_front();
+            dq.pop_back();
+            swap(s[idx1], s[idx2]);
+        }
+        
 
         return s;
     }
