521f578e07e3df0404aa0257ac6eb739340db3db
Uttkarsh kesharwani <145669545+UttkarshKesharwani@users.noreply.github.com>
2025-12-20 21:01:39 +0530

    Time: 0 ms (100%), Space: 21.2 MB (26.37%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -2,24 +2,16 @@ class Solution {
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
 
-        unordered_map<int,int> mp; // <(i-j),ele> 
+        // check other solution
+
         int rows = matrix.size();
         int cols = matrix[0].size();
 
-        for(int j=0;j<cols;j++){
-            int key = 0-j;
-            mp[key] = matrix[0][j];
-        }
-
-        for(int i=1;i<rows;i++){
-            int key = i-0;
-            mp[key] = matrix[i][0];
-        }
-
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
-                int key = i-j;
-                if(matrix[i][j] != mp[key]) return false;
+                if(i-1<0 || j-1<0 || i-1>=rows || j-1>=cols) continue;  // out of bond access
+                // if we check top-left neighbour for each element and it must be same 
+                if(matrix[i][j] !=  matrix[i-1][j-1]) return false;  
             }
         }
 
